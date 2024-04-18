84511e0c2e3590d03dc5c3c04baeb97d96d622f8
Bruno Trinitario <brunotrinitario@gmail.com>
2024-04-18 19:45:44 -0300

    Comienzo de impl de metricas

--- src/servidor/Metrica.java
+++ src/servidor/Metrica.java
@@ -1,5 +1,24 @@
 package servidor;
 
-public class Metrica {
+import java.util.ArrayList;
+
+import cliente.Cliente;
+import empleado.Empleado;
+
+public class Metrica implements iMetrica {
+	private ArrayList<Empleado> empleados=new ArrayList<Empleado>();
+	private ArrayList<Cliente> clientesAtendidos=new ArrayList<Cliente>();
+	//cliente tiene que tener el atributo tiempo de inicio y tiempo de atencion
+	@Override
+	public void consultarRendimiento() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buscaMetricas(ArrayList<Empleado> listaEmpleado, ArrayList<Metrica> listaMetricas) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
