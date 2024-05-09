bcb4cde327d3821daa16fa62b174ffc26d49a7c3
juaaniolave <juaaniolave@gmail.com>
2024-05-09 16:43:35 -0300

    vista servidor

--- src/main/MainServer.java
+++ src/main/MainServer.java
@@ -1,7 +1,9 @@
 package main;
 
+import controlador.ControladorServidor;
 import servidor.Servidor;
 import util.Constantes;
+import vista.VistaServidor;
 
 public class MainServer {
 
@@ -9,6 +11,7 @@ public class MainServer {
 		
 		Servidor servidor = new Servidor(Constantes.PUERTO);
 		servidor.start();
-
+		VistaServidor vistaServidor = new VistaServidor(1, Constantes.PUERTO);
+		ControladorServidor controladorServidor = new ControladorServidor(vistaServidor, servidor);		
 	}
 }
--- src/main/MainServer2.java
+++ src/main/MainServer2.java
@@ -1,14 +1,18 @@
 package main;
 
+import controlador.ControladorServidor;
 import servidor.Servidor;
 import util.Constantes;
+import vista.VistaServidor;
 
 public class MainServer2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Servidor servidor = new Servidor(Constantes.PUERTO2);
 		servidor.start();
+		VistaServidor vistaServidor = new VistaServidor(2, Constantes.PUERTO2);
+		ControladorServidor controladorServidor = new ControladorServidor(vistaServidor, servidor);	
 	}
 
 }
